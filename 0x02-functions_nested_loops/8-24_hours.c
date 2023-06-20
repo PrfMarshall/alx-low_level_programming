ba0e502e7dba2ef2001680888d31b0db7c725219
ILYASS BENTEKOUK <ilyassbentekouk@gmail.com>
2023-06-20 15:20:53 -0700

    trump

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,33 +1,29 @@
 #include "main.h"
+
 /**
- * jack_bauer - as the name of the function
- *
- * Return: nothing.
+ * jack_bauer - prints every minute of the day of Jack Bauer
+ * starting from 00:00 to 23:59
  */
 void jack_bauer(void)
 {
-int a;
-int b;
-int c;
-int d;
-while(a+b+c+d == 19){
-_putchar(a);
-_putchar(b);
-_putchar(':');
-_putchar(c);
-_putchar(d);
-d++;
-if(d<10){
-d=0;
-c++;
-if(c<10){
-c=0;
-b++;
-if(b<10){
-b=0;
-a++;
+	int i, j;
+
+	i = 0;
+
+	while (i < 24)
+	{
+		j = 0;
+		while (j < 60)
+		{
+			_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
+			_putchar(':');
+			_putchar((j / 10) + '0');
+			_putchar((j % 10) + '0');
+			_putchar('\n');
+			j++;
+		}
+		i++;
+	}
 }
-}
-}
-}
-}
+
